05f7a9ea391977151941231b6add458258290493
Bruce <brucepark0@gmail.com>
2015-11-10 22:11:38 -0800

    Draw 6 rows of breakout blocks

--- brick.js
+++ brick.js
@@ -8,15 +8,27 @@ function Brick(strokeStyle, lineWidth, fillStyle) {
 Brick.prototype.draw = function(context) {
   context.fillStyle = this.fillStyle; //color
   context.fillRect(this.x, this.y, this.width, this.height); //rectangle shape
+  context.strokeRect(this.x, this.y, this.width, this.height); //outline
+  context.strokeStyle = this.strokeStyle;
+  context.lineWidth = this.lineWidth;
 }
 
 function drawBoard() {
   var canvas = $('canvas')[0];
-  this.context = canvas.getContext("2d");
-  var brick = new Brick("#0000FF", 2, "red");
-  brick.x = canvas.width / 3;
-  brick.y = canvas.height / 3;
-  brick.draw(this.context);
+  var rowLayers = ["red", "blue", "green", "purple"];
+  var context = canvas.getContext("2d");
+  var width = 100;
+  var height = 25;
+  for (var row = 0; row<6; row++) {
+    for (var col = 0; col<6; col++) {
+      var brick = new Brick("#000000", 2, rowLayers[row%4]);
+      brick.x = col*width;
+      brick.y = row*height;
+      brick.width = width;
+      brick.height = height;
+      brick.draw(context);
+    }
+  }
 }
 
 drawBoard();
